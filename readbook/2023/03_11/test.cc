153961d7210d2d4827583002a48279f02ec51a02
qukangjie <qukangjie@localhost.localdomain>
2023-04-07 15:01:07 +0800

    深入理解计算机系统 第二个

---
+++ readbook/2023/03_11/test.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout <<"hello bit" << std::endl;
+  return 0;
+}
---
+++ readbook/2023/03_30/test.cc
@@ -0,0 +1,13 @@
+// 分组交换 -- 就是把众多的报文集合出来,然后按照一定的顺序进行转发
+// TCP/IP 协议具有开放性和实用性两大特点
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  printf("总字节数 %lu\n", (unsigned long)pow(2.0, 32));
+  printf("%lu\n", (unsigned long)pow(2.0, 32));
+  //cout << pow(2.0, 32) << endl;
+  return 0;
+}
