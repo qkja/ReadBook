88a471ccfa5eb57c043339c1cad8c9361fe85189
qkja <qkj1205@163.com>
2023-05-09 23:25:49 +0800

    C++ Primer 第二章前半部分

---
+++ readbook/C++Primer/leosson01/test.cc
@@ -0,0 +1,106 @@
+/**
+* User: Qkj
+* Description: 
+* Date: 2023-05-04
+* Time: 15:36
+*/
+
+
+
+
+
+//#include <iostream>
+//#include <string>
+//#include <fstream>
+//
+//int main()
+//{
+//  std::string file_name = "file.txt";
+//  std::ofstream out_file(file_name); // 打开一个文件供输出
+//  
+//  if(out_file.is_open() == false)
+//  {
+//    std::cerr << "打开文件失败" << std::endl;
+//    return 0;
+//  }
+//
+//  std::string str = "为了测试";
+//  for(int i = 0; i < 3; i++)
+//    out_file << str;
+//  return 0;
+//}
+//
+
+
+
+//int main()
+//{
+//  std::string str;
+//  //std::cin >> str;
+//  std::getline(std::cin, str);
+//  std::cout << str << std::endl;
+//  return 0;
+//}
+
+
+
+
+
+
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//  int a = 0;
+//  std::cin >> a;
+//  std::cout << a;
+//  std::cout << std::endl; // endl 就是换行符
+//  std::cerr << a;
+//  std::cout << std::endl; // endl 就是换行符
+//  return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//#include <cstdio>
+//#include <cassert>
+//#include <iostream>
+
+
+
+
+
+
+
+//int main()
+//{
+// 
+//  
+//  //std::assert(0);
+//  assert(0);
+//  return 0;
+//}
+
+
+
+//int main()
+//{
+//
+//
+//  //std::cout << "Error: " << __FILE__ << " : line " 
+//  //  << __LINE__ << "element_count must be non-zero.\n";
+//
+////  std::cout << "编译时间 " << __TIME__  << std::endl;
+// // std::cout << "日期 " << __DATE__ << std::endl;
+//
+////#ifdef DEBUG 
+////  std::cout <<"宏已经被定义" << std::endl;
+////#else 
+////  std::cout <<"宏没有被定义" << std::endl;
+////#endif
+//  return 0;
+//}
