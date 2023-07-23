593af5b4628f6e478db0db7c9f5103e72c89303a
qkja <qkj1205@163.com>
2023-07-23 16:24:25 +0800

    提交代码

---
+++ interview/C++/2023_05_10/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  // \t  应该是退格符
+  printf("aaaa\tbbb");
+  return 0; 
+}
---
+++ interview/C++/2023_05_10/test.cc
@@ -0,0 +1,12 @@
+/**
+* User: Qkj
+* Description: 
+* Date: 2023-05-10
+* Time: 23:20
+*/
+
+int main()
+{
+  
+  return 0;
+}
