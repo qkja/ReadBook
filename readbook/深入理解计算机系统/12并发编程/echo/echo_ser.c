4eabf5a97b887adb62d1c42a4c2868c51512d998
qkja <qkj1205@163.com>
2023-08-14 22:52:16 +0800

    僵尸进程解决的不好

--- readbook/深入理解计算机系统/12并发编程/echo/echo_ser.c
+++ readbook/深入理解计算机系统/12并发编程/echo/echo_ser.c
@@ -5,12 +5,75 @@
  * Time: 23:27
  */
 #include <stdio.h>
+#include <unistd.h>
 #include "../open_fd/open_listenfd.h"
 #include "task.hpp"
+#include <sys/types.h>
+#include <sys/wait.h>
+
 // ./server server_port
 // 1 单进程版本
+// int main(int argc, char **argv)
+// {
+//   if (argc != 2)
+//   {
+//     printf("Usage:\n\t./server server_port\n");
+//     exit(1);
+//   }
+//   int listen_fd = -1;
+//   int port = atoi(argv[1]);
+//   struct sockaddr_in clientaddr;
+//   listen_fd = open_listenfd(port);
+//   printf("监听套接字打开成功%d\n", listen_fd);
+//   while (1)
+//   {
+//     socklen_t client_len = sizeof(clientaddr);
+//     // 创建链接描述符
+//     int connfd = accept(listen_fd, (struct sockaddr *)&clientaddr, &client_len);
+//     printf("open fd %d success\n", connfd);
+//     close(connfd);
+
+//     printf("close fd %d success\n", connfd);
+//     // printf("=======================\n");
+//     // if(connfd == )先不关系创建失败的问题
+//     //  处理任务
+//     // 1 单进程版本
+//     while (1)
+//     {
+//       if (echo(connfd) == -1)
+//         break;
+//     }
+//   }
+//   return 0;
+// }
+// 2.多进程版本,非阻塞版本
+
+void wait_all_id(int *ids, int len, int id)
+{
+  for (int i = 0; i < 10; i++)
+  {
+    if (ids[i] == -1)
+    {
+      ids[i] = id;
+      break;
+    }
+  }
+
+  for (int i = 0; i < len; i++)
+  {
+    if (ids[i] != -1)
+    {
+      int ret = waitpid(ids[i], NULL, WNOHANG);
+      if (ret > 0)
+      {
+        ids[i] = -1;
+      }
+    }
+  }
+}
 int main(int argc, char **argv)
 {
+
   if (argc != 2)
   {
     printf("Usage:\n\t./server server_port\n");
@@ -20,25 +83,49 @@ int main(int argc, char **argv)
   int port = atoi(argv[1]);
   struct sockaddr_in clientaddr;
   listen_fd = open_listenfd(port);
+  int ids[10] = {-1};
+  memset(ids, -1, sizeof(ids));
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d ", ids[i]);
+  }
   printf("监听套接字打开成功%d\n", listen_fd);
+
   while (1)
   {
     socklen_t client_len = sizeof(clientaddr);
     // 创建链接描述符
     int connfd = accept(listen_fd, (struct sockaddr *)&clientaddr, &client_len);
-    printf("open fd %d success\n", connfd);
-    close(connfd);
-
-    printf("close fd %d success\n", connfd);
-    // printf("=======================\n");
-    // if(connfd == )先不关系创建失败的问题
-    //  处理任务
-    // 1 单进程版本
-    while (1)
+    // 创建进程
+    pid_t id = fork();
+    if (id == 0)
     {
-      if (echo(connfd) == -1)
-        break;
+      close(listen_fd);
+      // printf("我是子进程 %d\n", getpid());
+      // // 子进程
+      while (1)
+      {
+        if (echo(connfd) == -1)
+          break;
+      }
+      close(connfd);
+      return 0;
     }
+    else if (id < 0)
+    {
+      // 错误
+      continue;
+    }
+
+    // printf("我是父进程 %d\n", id);
+    // 这里一定是父进程
+    close(connfd);
+    // printf("我是父进程 %d\n", id);
+
+    wait_all_id(ids, sizeof(ids) / sizeof(ids[0]), id);
   }
+
+  wait_all_id(ids, sizeof(ids) / sizeof(ids[0]), -1);
+  close(listen_fd);
   return 0;
 }
