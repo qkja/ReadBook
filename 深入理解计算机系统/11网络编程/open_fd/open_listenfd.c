b9298c3573cd60597648a490f5be7e5c7a14c2f1
qkja <qkj1205@163.com>
2023-09-10 02:34:38 -0700

    提交

--- 深入理解计算机系统/11网络编程/open_fd/open_listenfd.c
+++ 深入理解计算机系统/11网络编程/open_fd/open_listenfd.c
@@ -1,9 +1,9 @@
 /**
-* User: Qkj
-* Description: 
-* Date: 2023-08-13
-* Time: 22:51
-*/
+ * User: Qkj
+ * Description:
+ * Date: 2023-08-13
+ * Time: 22:51
+ */
 // 返回一个监听套接字
 #include "open_listenfd.h"
 
@@ -13,13 +13,12 @@ int open_listenfd(int port)
   int optval = 1;
   struct sockaddr_in server;
 
-
   // 1. 创建套接字
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(listenfd < 0)
+  if (listenfd < 0)
     exit(-1);
   // 2. 让服务器立即重启
-  if(setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval, sizeof(int)) < 0)
+  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int)) < 0)
     exit(-2);
 
   // 3. 填充服务器信息
@@ -27,13 +26,13 @@ int open_listenfd(int port)
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = htonl(INADDR_ANY);
   server.sin_port = htons((unsigned short)port);
-  
+
   // 4. 绑定 服务器信息和套接字
-  if(bind(listenfd, (const struct sockaddr*)&server, sizeof(server)) < 0)
+  if (bind(listenfd, (const struct sockaddr *)&server, sizeof(server)) < 0)
     exit(-3);
 
   // 5. 监听 把套接字变为监听套接字
-  if(listen(listenfd, 50) < 0)
+  if (listen(listenfd, 50) < 0)
     exit(-4);
   return listenfd;
 }
