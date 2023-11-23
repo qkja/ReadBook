d9693b763ab43b9f788221dc245d0a35f2a433b3
qkja <qkja1205@163.com>
2023-11-23 19:38:23 +0800

    提交

---
+++ C Primer Plus(第五版)/02 C语言概述/02 C语言概述/test.c
@@ -0,0 +1,112 @@
+//#include <stdio.h>
+
+//int main()
+//{
+//	int words = 3020;
+//	int lines = 350;
+//	printf("There were %d words and %d lines", words, lines);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	const char* surname = "张";
+//	const char* name = "三";
+//	printf("%s %s\n", surname, name);
+//	printf("%s\n%s\n", surname, name);
+//	printf("%s ", surname);
+//	printf("%s\n", name);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	const char* name = "张三";
+//	const char* address = "徐州";
+//
+//	printf("%s %s\n", name, address);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int year = 10;
+//	int days = 365 * year;
+//
+//	printf("%d %d\n", year, days);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void f1()
+//{
+//	printf("1\n");
+//	printf("1\n");
+//	printf("1\n");
+//}
+//void f2()
+//{
+//	printf("2\n");
+//}
+//int main()
+//{
+//	f1();
+//	f2();
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int toes = 0;
+//	toes = 10;
+//	printf("toes = %d sum = %d cubic = %d", toes, toes + toes, toes * toes * toes);
+//	return 0;
+//}
+#include <stdio.h>
+//void f()
+//{
+//	printf("smail");
+//}
+//int main()
+//{
+//	f();
+//	f();
+//	f();
+//	printf("\n");
+//	f();
+//	f();
+//	printf("\n");
+//	f();
+//	printf("\n");
+//	return 0;
+//}
+//void one()
+//{
+//	printf("one\n");
+//}
+//void two()
+//{
+//	printf("two\n");
+//}
+//int main()
+//{
+//	
+//	printf("string now\n");
+//	one();
+//	two();
+//	printf("three\n");
+//	printf("done\n");
+//	return 0;
+//}
+
+
---
+++ C Primer Plus(第五版)/03 数据和C/03 数据和C/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("qqqq");
+//	printf("\a");
+//	printf("qqqq");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%o\n", '1');
+//	char ch = '\061';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//#include <inttypes.h>
+int main()
+{
+	printf("%d %f", 7.00, 7.0f);
+	return 0;
+}
